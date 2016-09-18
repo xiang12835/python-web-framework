314acca6cc9265abbfdf4f12794e4c27b28c9531
yuxiang <yuxiang@youku.com>
2016-09-18 11:19:45 +0800

    settings and .gitignore

--- my_django/myProject/settings.py
+++ my_django/myProject/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calc',
     'polls',
-    'learn',
+    'learning',
     'rest_framework',
 )
 
