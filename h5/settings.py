30b2c99a354ba7d2fb241c9727b6aa708ed0a53a
yu.xiang <yu.xiang@alibaba-inc.com>
2018-09-13 00:45:51 +0800

    update_h5_statics

--- h5/settings.py
+++ h5/settings.py
@@ -11,5 +11,5 @@ def load_tonardo_settings(tonardo_settings={}):
     tonardo_settings.update({
             'cookie_secret': '3f85eb25881ddd5c31f715542ae856c4',
             'template_path': os.path.join(os.path.dirname(__file__), "templates"),
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "static_path": os.path.join(os.path.dirname(__file__), "statics"),
     })
